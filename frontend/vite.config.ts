14d5afad0b96acbafd3f76fd23c9cc451d7d5d41
yuto-matsuda <yutochin.dev@gmail.com>
2026-05-30 15:45:48 +0900

    configure pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  build: {
+    outDir: 'docs',
+  },
+
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: true,
